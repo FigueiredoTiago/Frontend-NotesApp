a93ef8db38ef0c2d0a3c2549d48fb2f4ac5e1ea5
TiagoDev <developertiagof@gmail.com>
2024-01-18 17:51:31 -0300

    style(header): melhorias no layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,17 +7,19 @@ const Header = () => {
   return (
     <header>
 
-      <div className="logo-box">
-          <img src={logo} alt="logo" className="logo" />
-          <h1 className="tittle">CoreNotes</h1>
-      </div>
+      <nav className="container">
 
-      <div className="search">
-        <img src={search} alt="icon search" className="icon-search" />
-        <input type="text" placeholder="Pesquisar notas"  />
-      </div>
-
-      <img src={close} alt="close icon" className="close-btn" />
+        <div className="logo-box">
+            <img src={logo} alt="logo" className="logo" />
+            <h1 className="tittle">CoreNotes</h1>
+        </div>
+        <div className="search">
+          <img src={search} alt="icon search" className="icon-search" />
+          <input type="text" placeholder="Pesquisar notas"  />
+        </div>
+        <img src={close} alt="close icon" className="close-btn" />
+        
+      </nav>
 
     </header>
   );
