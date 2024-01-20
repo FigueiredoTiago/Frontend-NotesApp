80657678b1335b6d1088527b6ffcf112fbda1120
TiagoDev <developertiagof@gmail.com>
2024-01-20 18:10:45 -0300

    styles(card) Estilos dos cards melhorados

--- src/components/NoteCard/NoteCard.tsx
+++ src/components/NoteCard/NoteCard.tsx
@@ -1,16 +1,24 @@
 import "./styles.scss";
 import star from "../../assets/img/star.png";
+import starGold from "../../assets/img/starGold.png";
 import color from "../../assets/img/color.svg";
 import edit from "../../assets/img/edit.svg";
 import { NoteCardProps } from "../../interfaces/notes.interface";
 
 const NoteCard: React.FC<NoteCardProps> = ({ note }) => {
+  const getBackgroundColor = () => {
+    return { backgroundColor: note.color };
+  };
   return (
-    <div className="card grid-4">
+    <div className="card grid-4" style={getBackgroundColor()}>
       <div className="title-card">
         <h3 className="title">{note.title}</h3>
 
-        <img src={star} alt="star icon" />
+        {note.favorite ? (
+          <img src={starGold} alt="star golden icon" />
+        ) : (
+          <img src={star} alt="star icon" />
+        )}
       </div>
 
       <p className="note-text">{note.description}</p>
