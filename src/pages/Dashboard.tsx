944cff1d795ee0f3bad06640424f255f7eb4ff63
TiagoDev <developertiagof@gmail.com>
2024-01-20 17:57:17 -0300

    feat(showFavorites) notas favoritas  sao mostradas na tela

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,12 @@
 import NewNote from "../components/NewNote/NewNote";
 import NoteCard from "../components/NoteCard/NoteCard";
 import "./styles.scss";
-import { useApi } from "../services/API";
+import { useGetNotes, useGetFavorites } from "../services/API";
 import { Note } from "../interfaces/notes.interface";
 
 const Dashboard = () => {
-  const { data, loading } = useApi();
+  const { data, loading } = useGetNotes();
+  const { data: favorites } = useGetFavorites();
 
   console.log(data);
 
@@ -14,25 +15,24 @@ const Dashboard = () => {
   }
 
   return (
-    <main>
+    <main className="">
       <section className="create-note-section">
         <NewNote />
       </section>
 
       {/* section para mostrar apenas os favoritos */}
 
-      <section className="favorite-section">
+      <section className="favorite-section container">
         <p className="section-title">Favoritas</p>
-        {/* Renderize os favoritos aqui */}
+        {favorites?.map((item: Note) => <NoteCard key={item.id} note={item} />)}
       </section>
 
       {/* section para mostrar todos os itens da api */}
 
-      <section className="other-section">
+      <section className="other-section container">
         <p className="section-title">Outras</p>
 
         {data?.map((item: Note) => <NoteCard key={item.id} note={item} />)}
-        
       </section>
     </main>
   );
--- src/services/API.ts
+++ src/services/API.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios, { AxiosResponse } from "axios";
 import { Note } from "../interfaces/notes.interface";
 
-export const useApi = () => {
+export const useGetNotes = () => {
   const [data, setData] = useState<Note[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const apiUrl = "http://localhost:3000/notes/";
@@ -24,4 +24,27 @@ export const useApi = () => {
   }, [apiUrl]);
 
   return { data, loading, setData } as const;
+};
+export const useGetFavorites = () => {
+  const [data, setFavorite] = useState<Note[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const apiUrl = "http://localhost:3000/notes/favorites";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response: AxiosResponse<{ notes: Note[] }> =
+          await axios.get(apiUrl);
+        setFavorite(response.data.notes);
+        setLoading(false);
+      } catch (error) {
+        console.error(`Error fetching data from ${apiUrl}:`, error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [apiUrl]);
+
+  return { data, loading, setFavorite } as const;
 };
