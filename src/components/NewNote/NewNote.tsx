ae8b5086f30d50af2b17e7997976fd78c59fd380
TiagoDev <developertiagof@gmail.com>
2024-01-21 15:43:24 -0300

    feat(pick color) botao de selecao de cores adc

--- src/components/NewNote/NewNote.tsx
+++ src/components/NewNote/NewNote.tsx
@@ -2,7 +2,7 @@ import "./styles.scss";
 import star from "../../assets/img/star.png";
 import starGold from "../../assets/img/starGold.png";
 import submit from "../../assets/img/submit.png";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { useState } from "react";
 import { FormData } from "../../interfaces/notes.interface";
 import { useCreateNote } from "../../services/API";
@@ -15,6 +15,7 @@ const NewNote: React.FC<NewNoteCardProps> = ({ updateNewNoteList }) => {
     handleSubmit,
     formState: { errors },
     reset,
+    control,
   } = useForm<FormData>();
 
   const [favorite, setFavorite] = useState(false);
@@ -27,10 +28,12 @@ const NewNote: React.FC<NewNoteCardProps> = ({ updateNewNoteList }) => {
     const newNote: Note = {
       title: data.title,
       description: data.description,
-      color: "#29ffc9",
+      color: data.color,
       favorite: favorite,
       _id: "",
     };
+
+    console.log(data);
 
     await createNote(newNote);
 
@@ -42,6 +45,21 @@ const NewNote: React.FC<NewNoteCardProps> = ({ updateNewNoteList }) => {
   const handleStarClick = () => {
     setFavorite(!favorite);
   };
+
+  const coresPreDefinidas = [
+    { valor: "#BAE2FF" },
+    { valor: "#B9FFDD" },
+    { valor: "##FFE8AC" },
+    { valor: "#FFCAB9" },
+    { valor: "#F99494" },
+    { valor: "#9DD6FF" },
+    { valor: "#ECA1FF" },
+    { valor: "#DAFF8B" },
+    { valor: "#FFA285" },
+    { valor: "#CDCDCD" },
+    { valor: "#979797" },
+    { valor: "#A99A7C" },
+  ];
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="new-note">
@@ -58,9 +76,35 @@ const NewNote: React.FC<NewNoteCardProps> = ({ updateNewNoteList }) => {
         className="input-note"
       />
 
+      <Controller
+        name="color"
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <div className="color-box">
+            <select
+              {...field}
+              style={{
+                backgroundColor: field.value,
+              }}
+            >
+              {coresPreDefinidas.map((cor) => (
+                <option
+                  key={cor.valor}
+                  value={cor.valor}
+                  style={{
+                    backgroundColor: cor.valor,
+                  }}
+                ></option>
+              ))}
+            </select>
+          </div>
+        )}
+      />
+
       <button
         type="button"
-        className={` star-icon star-icon-button ${favorite ? "starred" : ""}`}
+        className={"star-icon star-icon-button"}
         onClick={handleStarClick}
       >
         {favorite ? (
